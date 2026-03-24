5d77ac8868bc1a6f843281d54a0d2a6f9007a020
Patnala Uday Kumar <udaypatnala5@gmail.com>
2026-03-24 17:30:59 +0530

    Day 8: Firebase setup completed

---
+++ dashboard-app/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoqOuUWQFQEt76hXc-ME4_rotYsaN2GS8",
+  authDomain: "dashboard-app-6c28e.firebaseapp.com",
+  projectId: "dashboard-app-6c28e",
+  storageBucket: "dashboard-app-6c28e.firebasestorage.app",
+  messagingSenderId: "244458687347",
+  appId: "1:244458687347:web:b63c972e1235231f031306"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
